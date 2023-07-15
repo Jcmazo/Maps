5ce7bce62314c6d2743f72361ac74db60fc8bd11
jcmazo <josephpoets@gmail.com>
2023-07-14 22:46:38 -0500

    add:rooter

--- src/route/geocode.js
+++ src/route/geocode.js
@@ -2,5 +2,8 @@ const geocodeController = require('../controllers/geocode')
 
 async function geocodeRoute(app) {
   app.get('/geocode',geocodeController.getGeocode)
+  app.get('/',(res,req)=>{
+    res.send('Hola mundo')
+  })
 }
 module.exports = geocodeRoute 

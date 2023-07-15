8582c974b90d1285ea45d093aa23008867aeef64
jcmazo <josephpoets@gmail.com>
2023-07-14 22:15:27 -0500

    feature:add refactor get geocode

--- src/controllers/geocode.js
+++ src/controllers/geocode.js
@@ -1,20 +1,20 @@
-const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode'
 const geoService = require('../services/geocode.js')
 
 const getGeocode = async (req, res) => {
   try {
     const { address } = req.query
-    const params = {
+
+    const dataGeocode = {
       address: encodeURIComponent(address),
       key: process.env.API_KEY
     }
-    const url = `${GEOCODE_URL}/json?address=${params.address}&key=${params.key}`
-    const geocode = await geoService.getGeocode(url)
+
+    const geocode = await geoService.getGeocode(dataGeocode)
     return res.json(geocode)
   }
   catch (err) {
     console.log(err)
-    return res.status(500).json(err)
+    return res.status(400).json(err)
   }
 }
 
--- src/services/geocode.js
+++ src/services/geocode.js
@@ -1,27 +1,37 @@
 const axios = require('axios')
 
-const getGeocode = async (url) => {
+const getGeocode = async (dataGeocode) => {
+
+  const { address, key } = dataGeocode
+  const googleBaseGeocodeUrl = process.env.GOOGLE_BASE_GEOCODE_URL
+  const url = `${googleBaseGeocodeUrl}/json?address=${address}&key=${key}`
+
   try {
     const response = await axios.get(url)
     const { results } = response.data
 
-    if (results.length > 0) {
-      const result = results[0]
-      const { formatted_address, geometry } = result
-      const { lat, lng } = geometry.location
-
-      const geocodeData = {
-        formatted_address: formatted_address,
-        latitude: lat,
-        longitude: lng
-      }
-      return geocodeData
-    } else {
+    if (results.length === 0) {
       throw new Error('No location found.')
     }
+
+    const dataResponse = mapperResponseGeocode(results[0])
+    return dataResponse
+
   } catch (error) {
     console.error('Geocoding request failed:', error.message)
     return { error: 'Geocoding request failed.' }
+  }
+}
+
+
+const mapperResponseGeocode = (resultGeocode) => {
+  const { formatted_address, geometry } = resultGeocode
+  const { lat, lng } = geometry.location
+
+  return {
+    formatted_address: formatted_address,
+    latitude: lat,
+    longitude: lng
   }
 }
 
